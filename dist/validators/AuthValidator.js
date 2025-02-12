7b496f9451b931952aca494e73aa4fc44005352d
.browserslistrcEvandro <evandrodejesuslima@gmail.com>
2025-02-12 13:31:19 -0300

    alterando validators js

--- dist/validators/AuthValidator.js
+++ dist/validators/AuthValidator.js
@@ -3,39 +3,33 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const express_validator_1 = __importDefault(require("express-validator"));
+
+const { body } = require("express-validator");
+
 exports.default = {
-    register: (0, express_validator_1.default)({
-        name: {
-            trim: true,
-            isLength: {
-                options: { min: 2 },
-            },
-            errorMessage: 'Nome precisa ter pelo menos 2 caracteres',
-        },
-        email: {
-            isEmail: true,
-            normalizeEmail: true,
-            errorMessage: 'Email inválido',
-        },
-        password: {
-            isLength: {
-                options: { min: 6 },
-            },
-            errorMessage: 'Senha precisa ter pelo menos 6 caracteres',
-        },
-    }),
-    login: (0, express_validator_1.default)({
-        email: {
-            isEmail: true,
-            normalizeEmail: true,
-            errorMessage: 'Email inválido',
-        },
-        password: {
-            isLength: {
-                options: { min: 6 },
-            },
-            errorMessage: 'Senha precisa ter pelo menos 6 caracteres',
-        },
-    }),
+    register: [
+        body('name')
+            .trim()
+            .isLength({ min: 2 })
+            .withMessage('Nome precisa ter pelo menos 2 caracteres'),
+
+        body('email')
+            .isEmail()
+            .normalizeEmail()
+            .withMessage('Email inválido'),
+
+        body('password')
+            .isLength({ min: 6 })
+            .withMessage('Senha precisa ter pelo menos 6 caracteres')
+    ],
+    login: [
+        body('email')
+            .isEmail()
+            .normalizeEmail()
+            .withMessage('Email inválido'),
+
+        body('password')
+            .isLength({ min: 6 })
+            .withMessage('Senha precisa ter pelo menos 6 caracteres')
+    ]
 };
